68fd495716afa515969169a9ef32dfb66213a466
Kenni Silverio <ksilverio31@gmail.com>
2019-01-04 13:28:12 -0500

    Created backend server that helps create a get and post request to the API and a helper function to get the result. Now working on the bonus features

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # InnovationTasks
 Get and Post requests to API
+
+Run npm install to add dependencies.
+Run npm run start to start the server.
+Open browser to localhost at port 8000.
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const parser = require('body-parser');
+const app = express();
+const axios = require('axios');
+
+//helper function for operations
+var getResult = (data) => {
+    if (data.operation === 'remainder'){
+        return data.left % data.right;
+    };
+    if (data.operation === 'subtraction'){
+        return data.left - data.right;
+    };
+    if (data.operation === 'addition'){
+        return data.left + data.right;
+    };
+    if (data.operation === 'multiplication'){
+        return data.left * data.right;
+    };
+    if (data.operation === 'division'){
+        return data.left / data.right;
+    };
+}
+
+// app.use(parser.json());
+
+app.get('/', (req, res) => {
+    // setInterval(() => {     
+    //   ---- trying to implement continuous get and post request but receiving error from resetting the headers multiple times on the express server.
+        axios.get('https://interview.adpeai.com/api/v1/get-task')
+        .then(({data}) => {
+            var result = getResult(data);
+            axios.post('https://interview.adpeai.com/api/v1/submit-task', {
+                id: data.id,
+                result: result
+            })
+            .then((response) => {
+                if (response.status === 200){
+                    return res.status(200).send(`${response.data}!! ${data.id} : The result of ${data.left} and ${data.right} using the operation ${data.operation} equals ${result}.`)
+                }
+                else if (response.status === 400){
+                    console.log('Incorrect value in result; no ID specified; value is invalid.')
+                    // return res.status(400).end()
+                }
+                else if (response.status === 500){
+                    console.log('ID cannot be found.')
+                    // return res.status(500).end()
+                }
+            })
+            .catch(err => console.log(err, 'this is error in post request to API'))
+        })
+        .catch(err => console.log(err, 'this is error in get request to API'))
+    // }, 3000)
+});
+
+
+
+app.listen(8000, () => {
+    console.log('Listening on Port 8000');
+});
