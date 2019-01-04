2cc4a1c0719b9a0e8539dc057d3be0289ec7cdf2
Kenni Silverio <ksilverio31@gmail.com>
2019-01-04 13:52:23 -0500

    added and commented out code to integrate continous getting and posting of requests to the api but ran into some errors. With the interest of time I will submit repo for review.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 const axios = require('axios');
-
 //helper function for operations
 var getResult = (data) => {
     if (data.operation === 'remainder'){
@@ -22,11 +21,36 @@ var getResult = (data) => {
     };
 }
 
-// app.use(parser.json());
+//   ---- trying to implement continuous get and post request but receiving error from resetting the headers multiple times on the express server. Might be best to run the following call using AJAX instead of axios along with jQuery.
+// var executeRequest = () => {
+//     axios.get('https://interview.adpeai.com/api/v1/get-task')
+//     .then(({data}) => {
+//         var result = getResult(data);
+//         axios.post('https://interview.adpeai.com/api/v1/submit-task', {
+//             id: data.id,
+//             result: result
+//         })
+//         .then((response) => {
+//             if (response.status === 200){
+//                 return res.status(200).send(`${response.data}!! ${data.id} : The result of ${data.left} and ${data.right} using the operation ${data.operation} equals ${result}.`)
+//             }
+//             else if (response.status === 400){
+//                 console.log('Incorrect value in result; no ID specified; value is invalid.')
+//                 // return res.status(400).end()
+//             }
+//             else if (response.status === 500){
+//                 console.log('ID cannot be found.')
+//                 // return res.status(500).end()
+//             }
+//         })
+//         .catch(err => console.log(err, 'this is error in post request to API'))
+//     })
+//     .catch(err => console.log(err, 'this is error in get request to API'))
+//     setInterval(executeRequest, 3000);
+// }
+
 
 app.get('/', (req, res) => {
-    // setInterval(() => {     
-    //   ---- trying to implement continuous get and post request but receiving error from resetting the headers multiple times on the express server.
         axios.get('https://interview.adpeai.com/api/v1/get-task')
         .then(({data}) => {
             var result = getResult(data);
@@ -50,7 +74,6 @@ app.get('/', (req, res) => {
             .catch(err => console.log(err, 'this is error in post request to API'))
         })
         .catch(err => console.log(err, 'this is error in get request to API'))
-    // }, 3000)
 });
 
 
